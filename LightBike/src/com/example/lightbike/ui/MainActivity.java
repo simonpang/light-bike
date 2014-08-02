9571cd2ffc59fe2c981f2bc4d0297a08f059afae
shwenzhang <shaowenzhang1986@gmail.com>
2014-08-02 19:31:34 +0800

    1. commit

--- LightBike/src/com/example/lightbike/ui/MainActivity.java
+++ LightBike/src/com/example/lightbike/ui/MainActivity.java
@@ -1,134 +1,72 @@
 package com.example.lightbike.ui;
 
-import java.io.File;
+import com.example.lightbike.R;
 
-import android.net.Uri;
-import android.os.Bundle;
-import android.content.ComponentName;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import com.example.lightbike.R;
-import com.example.lightbike.ble.BlunoLibrary;
-import com.example.lightbike.qr_codescan.QRCodeActivity;
 
-public class MainActivity  extends BlunoLibrary {
-	private Button buttonScan;
-	private Button buttonSerialSend;
-	private EditText serialSendText;
-	private TextView serialReceivedText;
+public class MainActivity  extends BaseActivity{
+
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		Intent intent = new Intent();
-		intent.setClass(MainActivity.this, QRCodeActivity.class);
+		setContentView(R.layout.activity_main);
+
+		Button btnBluetooth = (Button) findViewById(R.id.btn_bluetooth);
 		
-		finish();
-//		setContentView(R.layout.activity_main);
-//        onCreateProcess();														//onCreate Process by BlunoLibrary
-//        
-//        serialBegin(115200);													//set the Uart Baudrate on BLE chip to 115200
-//		
-//        shareToTimeLine(null);
-//        serialReceivedText=(TextView) findViewById(R.id.serialReveicedText);	//initial the EditText of the received data
-//        serialSendText=(EditText) findViewById(R.id.serialSendText);			//initial the EditText of the sending data
-//        
-//        buttonSerialSend = (Button) findViewById(R.id.buttonSerialSend);		//initial the button for sending the data
-//        buttonSerialSend.setOnClickListener(new OnClickListener() {
-//			
-//			@Override
-//			public void onClick(View v) {
-//				// TODO Auto-generated method stub
-//
-//				serialSend(serialSendText.getText().toString());				//send the data to the BLUNO
-//			}
-//		});
-//        
-//        buttonScan = (Button) findViewById(R.id.buttonScan);					//initial the button for scanning the BLE device
-//        buttonScan.setOnClickListener(new OnClickListener() {
-//			
-//			@Override
-//			public void onClick(View v) {
-//				// TODO Auto-generated method stub
-//
-//				buttonScanOnClickProcess();										//Alert Dialog for selecting the BLE device
-//			}
-//		});
+		btnBluetooth.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent();
+				intent.setClass(MainActivity.this, BluetoothUI.class);
+				startActivity(intent);
+			}
+		});
+		
+		Button btnMap = (Button) findViewById(R.id.btn_map);
+		
+		btnMap.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent();
+				intent.setClass(MainActivity.this, LocationUI.class);
+				startActivity(intent);
+			}
+		});
+		
+		Button btnQrscan = (Button) findViewById(R.id.btn_qrscan);
+		
+		btnQrscan.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent();
+				intent.setClass(MainActivity.this, QRCodeUI.class);
+				startActivity(intent);
+			}
+		});
+		
+
+		
+		
+		
+
 	}
 
 	protected void onResume(){
 		super.onResume();
-		System.out.println("BlUNOActivity onResume");
-		onResumeProcess();														//onResume Process by BlunoLibrary
+																//onResume Process by BlunoLibrary
 	}
 	
-	private void shareToTimeLine(File file) {  
-        Intent intent = new Intent();  
-        ComponentName componentName = new ComponentName("com.tencent.mm", "com.tencent.mm.ui.tools.ShareToTimeLineUI");  
-        intent.setComponent(componentName);  
-        intent.setAction(Intent.ACTION_SEND);  
-        intent.setType("image/*");  
-        intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(file));  
-        startActivity(intent);  
-    } 
 	
-	
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		onActivityResultProcess(requestCode, resultCode, data);					//onActivityResult Process by BlunoLibrary
-		super.onActivityResult(requestCode, resultCode, data);
-	}
-	
-    @Override
-    protected void onPause() {
-        super.onPause();
-        onPauseProcess();														//onPause Process by BlunoLibrary
-    }
-	
-	protected void onStop() {
-		super.onStop();
-		onStopProcess();														//onStop Process by BlunoLibrary
-	}
-    
-	@Override
-    protected void onDestroy() {
-        super.onDestroy();	
-        onDestroyProcess();														//onDestroy Process by BlunoLibrary
-    }
-
-	@Override
-	public void onConectionStateChange(connectionStateEnum theConnectionState) {//Once connection state changes, this function will be called
-		switch (theConnectionState) {											//Four connection state
-		case isConnected:
-			buttonScan.setText("Connected");
-			break;
-		case isConnecting:
-			buttonScan.setText("Connecting");
-			break;
-		case isToScan:
-			buttonScan.setText("Scan");
-			break;
-		case isScanning:
-			buttonScan.setText("Scanning");
-			break;
-		case isDisconnecting:
-			buttonScan.setText("isDisconnecting");
-			break;
-		default:
-			break;
-		}
-	}
-
-	@Override
-	public void onSerialReceived(String theString) {							//Once connection data received, this function will be called
-		// TODO Auto-generated method stub
-		serialReceivedText.append(theString);							//append the text into the EditText
-		//The Serial data from the BLUNO may be sub-packaged, so using a buffer to hold the String is a good choice.
-					
-	}
 
 }
