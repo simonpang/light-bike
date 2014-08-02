6c45930868b7c9ebfa84e8ccb202c96bf61e489c
simsun <sunsj1231@gmail.com>
2014-08-02 16:35:09 +0800

    fix crash when start app

--- LightBike/src/com/example/lightbike/BikeApplication.java
+++ LightBike/src/com/example/lightbike/BikeApplication.java
@@ -1,7 +1,12 @@
 package com.example.lightbike;
 
 import android.app.Application;
+import com.baidu.mapapi.SDKInitializer;
 
 public class BikeApplication extends Application {
-
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        SDKInitializer.initialize(this);
+    }
 }
