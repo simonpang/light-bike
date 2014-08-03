1cb1c29619f03c7b4cc9d59ce884fde727f92e69
shwenzhang <shaowenzhang1986@gmail.com>
2014-08-03 09:25:50 +0800

    1.commit

--- LightBike/src/com/example/lightbike/ui/BaseActivity.java
+++ LightBike/src/com/example/lightbike/ui/BaseActivity.java
@@ -12,14 +12,19 @@ import android.view.MenuItem;
  * Created by sim on 8/2/14.
  */
 public class BaseActivity extends Activity {
+    /* (non-Javadoc)
+     * @see android.app.Activity#onCreate(android.os.Bundle)
+     */
     @SuppressLint("NewApi")
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ActionBar actionBar = getActionBar();
         //actionBar.setBackgroundDrawable(getResources().getDrawable(R.drawable.ab_background));
-        actionBar.setDisplayHomeAsUpEnabled(true);
+//        actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setBackgroundDrawable(getResources().getDrawable(R.drawable.background_green));
+        actionBar.setIcon(getResources().getDrawable(R.drawable.icon_qrscan));
+        
     }
 
     @Override
--- LightBike/src/com/example/lightbike/ui/LocationUI.java
+++ LightBike/src/com/example/lightbike/ui/LocationUI.java
@@ -3,6 +3,8 @@ package com.example.lightbike.ui;
 import android.graphics.Point;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -209,4 +211,23 @@ public class LocationUI extends BaseActivity {
 
         }
     }
+    
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.screen_scan, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.scanner_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 }
